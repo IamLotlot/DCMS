cedb09c6d56903e6e18130c4dcaab9dbb2444d53
Lotlot <jcnavea3@gmail.com>
2023-05-28 13:32:37 +0800

    add folders

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="icon" href="img">
+	<link rel="icon" type="image/x-icon" href="css/img/StudyBuddy.ico">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="css/home.css">
+    <script src="https://kit.fontawesome.com/8ef5e4d9da.js"></script>
+    <title>DCMS | Home Page</title>
+</head>
+<body>
+    
+</body>
+</html>
